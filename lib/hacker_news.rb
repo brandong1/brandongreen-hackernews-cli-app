e7b946fc2a4ae4c3be268a34f48085a815be4353
Brandon Waiters <brandon@brandonleeproductions.com>
2018-07-26 23:54:11 -0600

    Created Executable. Required all the things. Created Article class.

--- lib/hacker_news.rb
+++ lib/hacker_news.rb
@@ -1,3 +1,11 @@
+#Environment File
+
+require 'nokogiri'
+require 'open-uri'
+require 'pry'
+require_relative "./hacker_news/version"
+require_relative "./hacker_news/cli"
+require_relative "./hacker_news/article"
 require "hacker_news/version"
 
 module HackerNews
---
+++ lib/hacker_news/article.rb
@@ -0,0 +1,1 @@
+class HackerNews::Article
