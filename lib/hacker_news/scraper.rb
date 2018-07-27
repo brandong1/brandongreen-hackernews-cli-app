9a817835a0c6ffe141b382e8a591e678f62d7ea1
Brandon Waiters <brandon@brandonleeproductions.com>
2018-07-27 00:37:46 -0600

    Finished Scraper Class.

--- lib/hacker_news/scraper.rb
+++ lib/hacker_news/scraper.rb
@@ -7,9 +7,18 @@ class HackerNews::Scraper
 
         #Iterate over each article
         doc.css('.title').each do |article_doc|
-            title = article_doc.css('a')attribute('href').value
-
+            title = article_doc.css('.title').text.strip #DEBUG: Does it need to be title = article_doc.css('.title a').text.strip
+            url = article_doc.css('a').attribute('href').value
             #Create new instance of Article
             HackerNews::Article.new(title, url)
         end
+
+    def self.scrape_article_description(article)
+        url = article.url
+        doc = Nokogiri::HTML(open(BASE_URL))
+
+        #Scrape the article text
+        article.description = doc.css('div.dateline') #DEBUG: div.class? or span class="descriptor"
+    end
+    
 end
