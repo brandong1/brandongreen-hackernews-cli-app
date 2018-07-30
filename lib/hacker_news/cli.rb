181eda2b6c0236685ca37ec17fd430dc2a20c676
Brandon Waiters <brandon@brandonleeproductions.com>
2018-07-30 10:19:09 -0600

    Refactoring code to match new category to scrape. Created Jobs class.

--- lib/hacker_news/cli.rb
+++ lib/hacker_news/cli.rb
@@ -16,60 +16,59 @@ class HackerNews::CLI
     end  
 
     def main_menu
-        puts "Please type 'news' for a list of news articles or type 'jobs' to show available jobs."
+        puts "Please type 'news' for a list of news headlines or type 'jobs' to show available jobs."
         puts ""
 
-        #Show the list of scraped articles
         
-
-        # Iterate over scraped articles array
-        # DEBUG: cli.rb:26:in `main_menu': undefined local variable or method `articles'
-        #articles = Nokogiri::HTML(open(BASE_URL))
-        #HackerNews::Articles.all.each.with_index do |article, index| # Could also do each.with_index(1?)[1?]
-            #articles.css('.title a')
-            
-            #puts "#{index + 1}. #{article.title}"
-        #end
-
-        #articles = HackerNews::Article.all
- 
-        #puts "Please select an article to read more: "
-        
-        #articles = HackerNews::Article.all
-
         input = nil
         until input == "exit"
             input = gets.strip
             case input
             when "news"
-                puts "hi"
+                puts "list of news headlines"
             when "jobs"
-                puts "jobs"
+                puts "list of available jobs"
             when "exit"
                 exit
             end
             
         end
-
+        
         # Input to index minus 1 so user input corresponds to proper array index
         #index = input.to_i -1 
-
+        
         #article = HackerNews::Article.all[index]
-
+        
         # Put this in a new method
         # This will prevent needing to scrape again if more than one selection is made.
-
+        
         if !article.description || !article.title
             HackerNews::Scraper.scrape_article_description
         end
-
+        
         puts article.title
         puts article.description
-
+        
         puts "============================="
         main_menu
-
+        
         #add an exit option
         #add behavior for invalid input
     end
 end
+
+#Show the list of scraped articles
+# Iterate over scraped articles array
+# DEBUG: cli.rb:26:in `main_menu': undefined local variable or method `articles'
+#articles = Nokogiri::HTML(open(BASE_URL))
+#HackerNews::Articles.all.each.with_index do |article, index| # Could also do each.with_index(1?)[1?]
+    #articles.css('.title a')
+    
+    #puts "#{index + 1}. #{article.title}"
+#end
+
+#articles = HackerNews::Article.all
+
+#puts "Please select an article to read more: "
+
+#articles = HackerNews::Article.all
--- lib/hacker_news/scraper.rb
+++ lib/hacker_news/scraper.rb
@@ -1,7 +1,8 @@
 class HackerNews::Scraper
 
-    BASE_URL = "https://news.ycombinator.com" # does this need to be in quotes?
+    BASE_URL = "https://news.ycombinator.com/newest" # does this need to be in quotes?
 
+    BASE_URL_JOBS = "https://news.ycombinator.com/jobs"
     
     def self.scrape_news
         doc = Nokogiri::HTML(open(BASE_URL))
@@ -10,29 +11,40 @@ class HackerNews::Scraper
         doc.css('.title a.storylink').each do |article_title|
             title = article_title.text 
             url = article_title.attribute('href').value
-            #binding.pry
             HackerNews::Article.new(title, url)
+            #binding.pry
 
         end
     end
-        #title = articles.css('.title a.storylink').text #DEBUG: Does it need to be title = article_doc.css('.title a').text.strip
 
-        # #Iterate over each article
-
-        # doc.css('.title').each do |article_doc|
-        #     url = article_doc.css('a').attribute('href').value
-            #binding.pry
-            #Create new instance of Article
-            #HackerNews::Article.new(title, url)
-            #binding.pry
-        #end
-
-    def self.scrape_article_description(article)
-        url = article.url
-        doc = Nokogiri::HTML(open(BASE_URL))
-
-        #Scrape the article text
-        article.description = doc.css('div.dateline') #DEBUG: div.class? or span class="descriptor"
+    def self.scrape_jobs
+        doc = Nokogiri::HTML(open(BASE_URL_JOBS))
+        doc.css('.title a.storylink').each do |job|
+            job = article_title.text 
+            #url = article_title.attribute('href').value
+            HackerNews::Article.new(title, url)
+            binding.pry
+            #HackerNews::Jobs.new(description)
+        end
     end
-    
 end
+
+#title = articles.css('.title a.storylink').text #DEBUG: Does it need to be title = article_doc.css('.title a').text.strip
+
+# #Iterate over each article
+
+# doc.css('.title').each do |article_doc|
+#     url = article_doc.css('a').attribute('href').value
+    #binding.pry
+    #Create new instance of Article
+    #HackerNews::Article.new(title, url)
+    #binding.pry
+#end
+
+# def self.scrape_article_description(article)
+#     url = article.url
+#     doc = Nokogiri::HTML(open(BASE_URL))
+
+#     #Scrape the article text
+#     article.description = doc.css('div.dateline') #DEBUG: div.class? or span class="descriptor"
+# end
