72899b952721cecbebe0e2635a192896a0548488
Brandon Waiters <brandon@brandonleeproductions.com>
2018-07-31 00:12:40 -0600

    Remove url from Jobs

--- lib/hacker_news/jobs.rb
+++ lib/hacker_news/jobs.rb
@@ -1,12 +1,12 @@
 class HackerNews::Jobs
 
-    attr_accessor :job_headline #:url
+    attr_accessor :job_headline, :url
 
     @@all = []
 
     def initialize(job_headline)
         @job_headline = job_headline
-        #@url = url #Listing url as a possible add on
+        @url = url #Listing url as a possible add on
         @@all << self
     end
 
@@ -16,11 +16,13 @@ class HackerNews::Jobs
 
     def self.list_jobs
         job_list =[]
-        self.all.each do |jobs|
+        self.all.each do |jobs, url|
             job_list << jobs.job_headline
+            job_list << jobs.url
         end
-        job_list.each do |jobs_headline|
-            puts jobs_headline
+        job_list.each do |jobs_headline, url|
+            puts "#{jobs_headline}"
+            #puts "#{url}"
             puts ""
             #binding.pry
         end        
